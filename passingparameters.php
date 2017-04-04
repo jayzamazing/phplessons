cf8765e24bafd29548b5b12b9c4e641f8c0284ca
jayzamazing <adrianjaylopez@gmail.com>
2017-04-04 00:03:00 -0400

    Covered passing parameters in functions.

---
+++ passingparameters.php
@@ -0,0 +1,8 @@
+<?php
+function whoIsAwesome($awesomeperson, $description) {
+  echo $awesomeperson." is ".$description."!!!!";
+}
+$awesomeGuy = "Jay";
+$describe = "awesome";
+whoIsAwesome($awesomeGuy, $describe);
+ ?>
