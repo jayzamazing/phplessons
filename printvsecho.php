697133a501aa868c1a2edda27fcb8ff860dbefef
jayzamazing <adrianjaylopez@gmail.com>
2017-04-04 23:21:40 -0400

    Covered print vs echo statements.

---
+++ printvsecho.php
@@ -0,0 +1,15 @@
+<?php
+print "This above all: to thine own self be true\n";
+print("The lady doth protest too much, methinks\n");
+
+echo  "Et", "tu", ",", "Brute", "!";
+echo  "Et", " tu", ", ", "Brute", "!";
+
+
+print "
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Morbi tincidunt commodo est. In sit amet eleifend dui.
+Nam euismod porta ultrices.
+";
+
+ ?>
