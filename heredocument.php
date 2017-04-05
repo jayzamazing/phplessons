8d4efc025ce08597aa77c929917bd846fe9a75fb
jayzamazing <adrianjaylopez@gmail.com>
2017-04-04 23:18:25 -0400

    Covered the here doc.

---
+++ heredocument.php
@@ -0,0 +1,20 @@
+<?php
+$famousQuote = <<<EOT
+    Be not afraid of greatness;
+    some are born great,
+    some achieve greatness,
+    and others have greatness thrust upon them.
+
+                          - William Shakespeare
+EOT;
+
+print <<<QUOTE
+    Electric communication will never be a substitute for the face of someone
+    who with their soul encourages another person to be brave and true.
+            - Charles Dickens
+
+$famousQuote
+QUOTE;
+
+
+ ?>
